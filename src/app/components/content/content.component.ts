e1ed38e524b424b0668881760ed77eb065ae3844
sguha-work <sguha1988.life@gmail.com>
2017-08-05 17:17:50 +0530

    initial commit

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-//import { Router, NavigationEnd } from '@angular/router';
+import {AjaxService} from './../../services/ajax.service';
 
 @Component({
   selector: 'content',
@@ -7,10 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./content.style.css']
 })
 export class ContentComponent {
-  constructor() {
-    // router.events.subscribe((val) => {
-    //     // see also 
-    //     console.log(val instanceof NavigationEnd) 
-    // });
+  private setNumber: number;
+  private questionNumber: number;
+  private questionDataSet: any;
+  public question: any;
+
+  constructor(private ajax: AjaxService) {
+    this.getAndStoreQuestionDataSet();
+    this.question = {};
+    window.addEventListener("hashchange", () => {
+      this.parseLocation(window.location.hash);
+    });
+  }
+
+  getAndStoreQuestionDataSet() {
+    this.ajax.call("assets/data/response-export.json").then((response) => {
+      // ajax call success
+      this.questionDataSet = response.data;
+      this.loadQuestion(0, 0);
+    }, () => {
+      // ajax call failed
+      console.log("error");
+    });
+  }
+  
+  parseLocation(location: string) {
+    let setqsnValue = location.split("/").pop().split("&");
+    this.setNumber = parseInt(setqsnValue[0].split("set=").pop());
+    this.questionNumber = parseInt(setqsnValue[1].split("qsn=").pop());
+    if(typeof this.setNumber !== "undefined" && typeof this.questionNumber !== "undefined") {
+      this.loadQuestion(this.setNumber, this.questionNumber);
+    }
+  }
+
+  loadQuestion(setNumber: number, questionNumber: number) {
+    this.question = this.questionDataSet[setNumber+1].Questions[questionNumber];
+    console.log(this.question);
   }
 }
