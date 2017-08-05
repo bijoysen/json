d158fcd328e19f17a8f627c1aaa4ec6e58f4bd24
sguha-work <sguha1988.life@gmail.com>
2017-08-05 18:46:33 +0530

    done

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,18 +1,38 @@
 import { Component } from '@angular/core';
-
+import { AfterViewInit } from '@angular/core';
 import {AjaxService} from './../../services/ajax.service';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'menu',
   templateUrl: './menu.template.html',
   styleUrls: ['./menu.style.css']
 })
-export class MenuComponent {
+export class MenuComponent  implements AfterViewInit {
   public model: any;
+  public activeSetNumber: number;
+  public activeQuestionNumber: number;
   constructor(private ajax: AjaxService) {
     this.model = {}
     this.model.data = [];
     this.loadQuestionData();
+    window.addEventListener("hashchange", () => {
+      this.markQuestionAndSet(window.location.hash);
+    });
+  }
+
+  markQuestionAndSet(location: string) {
+    if(location !== "") {
+      let setqsnValue = location.split("/").pop().split("&");
+      this.activeSetNumber = parseInt(setqsnValue[0].split("set=").pop());
+      this.activeQuestionNumber = parseInt(setqsnValue[1].split("qsn=").pop());
+      $("div[data-type='set']").removeClass("active");
+      $("div[data-type='set']").eq(this.activeSetNumber).addClass("active");
+     
+      let setDiv = $("div[data-type='set']").eq(this.activeSetNumber)[0];
+      $("li[data-type='question']").removeClass("active");
+      $("li[data-type='question']", setDiv).eq(this.activeQuestionNumber).addClass("active");
+    }
   }
 
   loadQuestionData() {
@@ -27,5 +47,8 @@ export class MenuComponent {
       // ajax call failed
       console.log("error");
     });
+  }
+  ngAfterViewInit() {
+
   }
 }
