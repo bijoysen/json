88966d8bd268f85c3554c6194cbe31a533253417
sguha-work <sguha1988.life@gmail.com>
2017-08-05 18:11:28 +0530

    done

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -43,5 +43,51 @@ export class ContentComponent {
   loadQuestion(setNumber: number, questionNumber: number) {
     this.question = this.questionDataSet[setNumber+1].Questions[questionNumber];
     console.log(this.question);
+    this.preparePrevNextButtonData(setNumber, questionNumber);
   }
+
+  public enablePrev: boolean;
+  public enableNext: boolean;
+
+  public prevHref: string;
+  public nextHref: string;
+
+  preparePrevNextButtonData(setNumber: number, questionNumber: number) {
+    this.enablePrev = true;
+    this.enableNext = true;
+    
+    if(setNumber === 0 && questionNumber === 0) {
+      this.enablePrev = false;
+      this.enableNext = true;
+    }
+
+    // setting next button
+    let nextSetNumber = setNumber;
+    let nextQuestionNumber = questionNumber+1;
+    if(nextQuestionNumber >= this.questionDataSet[setNumber+1].Questions.length) {
+      nextSetNumber += 1;
+      nextQuestionNumber = 0;
+      
+    }
+    if(nextSetNumber >= Object.keys(this.questionDataSet).length) {
+      this.nextHref = "google.com"
+    } else {
+      this.nextHref = "#/question/set="+nextSetNumber+"&qsn="+nextQuestionNumber;
+    }
+
+    // setting previous button
+    let previousSetNumber = setNumber;
+    let previousQuestionNumber = questionNumber-1;
+
+    if(previousQuestionNumber<0) {
+      previousSetNumber -= 1;
+      if(previousSetNumber<0) {
+        previousSetNumber = 0;
+      }
+      let prevSetQuestionLength = this.questionDataSet[previousSetNumber+1].Questions.length;
+      previousQuestionNumber = this.questionDataSet[previousSetNumber+1].Questions[prevSetQuestionLength-1]
+    }
+    this.prevHref = "#/question/set="+previousSetNumber+"&qsn="+previousQuestionNumber;    
+  }
+
 }
